e37ecc13635a21ed12c934681a1c402a3592daf7
João Albuquerque <jfportalb@gmail.com>
2018-12-06 22:01:38 -0200

    Update lu.cu

--- lu.cu
+++ lu.cu
@@ -76,6 +76,32 @@ void  luGPU(double *A, int n, int blockSize) {
 	}
 }
 
+
+void  luGPUPivot(double *A, int n, int blockSize) {
+	int i, n_blocos;
+	for (i = 0; i < n-1; i++) {
+		n_blocos = ((n-i-1) + blockSize -1) / blockSize;
+		dim3  g_blocos(n_blocos, n_blocos);
+		dim3  n_threads(blockSize,blockSize);
+		luCalcCol <<< n_blocos, blockSize >>>(A, n, i);
+		CUDA_SAFE_CALL(cudaGetLastError());
+		luCalcSub <<< g_blocos, n_threads >>>(A, n, i);
+		CUDA_SAFE_CALL(cudaGetLastError());
+	}
+}
+
+void  luGPUPivotEscal(double *A, int n, int blockSize) {
+	int i, n_blocos;
+	for (i = 0; i < n-1; i++) {
+		n_blocos = ((n-i-1) + blockSize -1) / blockSize;
+		dim3  g_blocos(n_blocos, n_blocos);
+		dim3  n_threads(blockSize,blockSize);
+		luCalcCol <<< n_blocos, blockSize >>>(A, n, i);
+		CUDA_SAFE_CALL(cudaGetLastError());
+		luCalcSub <<< g_blocos, n_threads >>>(A, n, i);
+		CUDA_SAFE_CALL(cudaGetLastError());
+	}
+}
 void fillMatrix(double* A, int n){
 	for (int i=0; i<n; i++){
 		for (int j=0; j<n; j++){
@@ -105,13 +131,12 @@ int  main(int argc, char** argv) {
 	double begin, end, timeSeq, timeCpuGpu, timeRunPar, timeGpuCpu;
 	char *inputFileName, *outputFileName;
 	
-	if(argc < 4) {
-		cerr << "Digite: "<< argv[0] <<" <Dimensão do bloco> <Arquivo de entrada> <Arquivo de saída>" << endl;
+	if(argc < 3) {
+		cerr << "Digite: "<< argv[0] <<" <Arquivo de entrada> <Arquivo de saída> [Dimensão do bloco] [p|e]" << endl;
 		exit(EXIT_FAILURE);
 	}
-	blockSize = atol(argv[1]);
-	inputFileName = argv[2];
-	outputFileName = argv[3];
+	inputFileName = argv[1];
+	outputFileName = argv[2];
 	
 	ifstream infile (inputFileName, ios::binary);
 	infile.read(reinterpret_cast<char *>(&n), sizeof(int));
@@ -124,44 +149,59 @@ int  main(int argc, char** argv) {
 	}
 	infile.read(reinterpret_cast<char *>(Aseq), matBytes);
 	infile.close();
-	
-	Apar = (double *) malloc(matBytes);
-	if ( Apar == NULL   ) {
-		cerr << "Memoria  insuficiente" << endl;
-		exit(EXIT_FAILURE);
+	if (argc > 3){
+		blockSize = atol(argv[3]);
+		Apar = (double *) malloc(matBytes);
+		if ( Apar == NULL   ) {
+			cerr << "Memoria  insuficiente" << endl;
+			exit(EXIT_FAILURE);
+		}
+
+		GET_TIME(begin);
+		CUDA_SAFE_CALL(cudaMalloc((void**) &Adevice, matBytes));
+		CUDA_SAFE_CALL(cudaMemcpy(Aseq, Adevice, matBytes, cudaMemcpyDeviceToHost));
+		GET_TIME(end);
+		timeCpuGpu = end-begin;
+
+		if (argc > 4){
+			if (argv[4][0] == 'e'){
+				GET_TIME(begin);
+				luGPUPivotEscal(Adevice, n, blockSize);
+				GET_TIME(end);
+			}else{
+				GET_TIME(begin);
+				luGPUPivot(Adevice, n, blockSize);
+				GET_TIME(end);
+			}
+		} else {
+			GET_TIME(begin);
+			luGPU(Adevice, n, blockSize);
+			GET_TIME(end);
+		}
+		timeRunPar = end-begin;
+
+		GET_TIME(begin);
+		CUDA_SAFE_CALL(cudaMemcpy(Apar, Adevice, matBytes, cudaMemcpyDeviceToHost));
+		GET_TIME(end);
+		timeGpuCpu = end-begin;
+		
+		CUDA_SAFE_CALL(cudaFree(Adevice));
+		free(Apar);
+	} else {
+		GET_TIME(begin);
+		luSeq(Aseq, n);
+		GET_TIME(end);
+		timeSeq = end-begin;
 	}
 	
-	GET_TIME(begin);
-	CUDA_SAFE_CALL(cudaMalloc((void**) &Adevice, matBytes));
-	CUDA_SAFE_CALL(cudaMemcpy(Aseq, Adevice, matBytes, cudaMemcpyDeviceToHost));
-	GET_TIME(end);
-	timeCpuGpu = end-begin;
-	
-	GET_TIME(begin);
-	luGPU(Adevice, n, blockSize);
-	GET_TIME(end);
-	timeRunPar = end-begin;
-	
-	GET_TIME(begin);
-	CUDA_SAFE_CALL(cudaMemcpy(Apar, Adevice, matBytes, cudaMemcpyDeviceToHost));
-	GET_TIME(end);
-	timeGpuCpu = end-begin;
-	
-	GET_TIME(begin);
-	luSeq(Aseq, n);
-	GET_TIME(end);
-	timeSeq = end-begin;
-	
-	CUDA_SAFE_CALL(cudaFree(Adevice));
 	free(Aseq);
-	free(Apar);
 	
-	checkResults(Aseq, Apar, n);
+// 	checkResults(Aseq, Apar, n);
 	
-	ofstream outfile (outputFileName, ios::binary);
-	outfile.write(reinterpret_cast<char *>(&n), sizeof(int));
-	outfile.write(reinterpret_cast<char *>(Aseq), matBytes);
-	outfile.close();
+// 	ofstream outfile (outputFileName, ios::binary);
+// 	outfile.write(reinterpret_cast<char *>(&n), sizeof(int));
+// 	outfile.write(reinterpret_cast<char *>(Aseq), matBytes);
+// 	outfile.close();
 	
 	printResults(n, timeSeq, timeCpuGpu, timeRunPar, timeGpuCpu);
 	
